7a4fe23f984f0c81abcb418ef2c8eb930e099920
Corey <xenios91@protonmail.com>
2022-05-21 17:48:20 +0000

    added another error catch

--- main.go
+++ main.go
@@ -40,7 +40,12 @@ func handleGetNotifications(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		notifications := notification.NewNotificationQueue().GetNotifications()
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(notifications)
+		err := json.NewEncoder(w).Encode(notifications)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "", http.StatusInternalServerError)
+			return
+		}
 	} else {
 		http.Error(w, "", http.StatusMethodNotAllowed)
 	}
